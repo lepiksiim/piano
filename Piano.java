63dec90844c8264bbb669aa44bfbda05a42cf29a
Siim <lepiksiim@gmail.com>
2015-12-09 12:39:49 +0200

    New file and first commit order

---
+++ Piano.java
@@ -0,0 +1,17 @@
+/** This program should create piano keyboard.
+ * If user clicks on the right key, correct sound should be heard.
+ */
+
+
+package Homework;
+
+
+
+public class Piano {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

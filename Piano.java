073edc849427e803c3d67c9da4c113308265fd67
Siim <lepiksiim@gmail.com>
2015-12-09 13:35:20 +0200

    Appletiga graafika

--- Piano.java
+++ Piano.java
@@ -2,16 +2,31 @@
  * If user clicks on the right key, correct sound should be heard.
  */
 
-
 package Homework;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JApplet;
 
 
-public class Piano {
+
+
+
+public class Piano extends JApplet{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	public void paint(Graphics g) {
+		g.setColor(Color.white);
+		int w = getWidth();
+		int h = getHeight();
+		
+		g.fillRect(0, 0, w, h);
+		
+		g.setColor(Color.black);
+		g.fillRect(100, 100, 20, 20);
 	}
 
 }

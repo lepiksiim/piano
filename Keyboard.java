ec3bb48e3fc345c8f46295a16874ac92d882a3a8
Siim <lepiksiim@gmail.com>
2015-12-10 19:06:18 +0200

    Keyboard Ready

--- Keyboard.java
+++ Keyboard.java
@@ -16,13 +16,28 @@ public class Keyboard extends JPanel {
 		super.paintComponent(g);
 		int width = getWidth();
 		int height = getHeight();
-
+		int x = 0;
 		g.setColor(Color.WHITE);
-		g.fill3DRect(0, 0, width/21, height, true);
+		for (int i = 0; i < 21; i++) {
+			g.fill3DRect(x, 0, width / 21, height, true);
+			x = x + width / 21;
+		}
+		int y = 37;
+		g.setColor(Color.BLACK);
+		for (int j = 0; j < 20; j++) {
+			if (j == 2 || j == 6 || j == 9 || j == 13 || j == 16) {
+				y = y + width / 21;
+				continue;
+			} else {
+				g.fill3DRect(y, 0, width / 40, height / 2, true);
+				y = y + width / 21;
+			}
+		}
+
 	}
 
 	public static void main(String[] args) {
-		JFrame window = new JFrame("Java!");
+		JFrame window = new JFrame("Piano Keyboard");
 		Keyboard content = new Keyboard();
 		window.setContentPane(content);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
